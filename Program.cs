23d042d5b08357046b14b2f0d6c5389edc1e96ae
Ed Lu <ed@megacrit.com>
2024-08-07 17:01:02 -0700

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Raylib_CsLo;
+using MegaCrit.STS2.Core.Utilities.SDF;
+
+namespace megasdf;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Image source = Raylib.LoadImage(args[0]);
+        Image destination = Raylib.ImageCopy(source);
+        SDFTextureGenerator.Generate(source, destination, 0, float.Parse(args[2]), 1f, RGBFillMode.Distance);
+        Raylib.ExportImage(destination, args[1]);
+    }
+}
